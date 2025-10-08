fc3717eaa06e1c735a12d4af0b3150d3740d3d22
Santiago Tenensaray <santytenensaray@gmail.com>
2025-10-07 21:04:34 -0500

    Formulario con operaciones aaritméticas

--- javascript/suma.js
+++ javascript/suma.js
@@ -1,24 +1,28 @@
-function sumar(e) { //Creamos la funcion sumar y le pasamos el evento
-
+function calcular(e) { //Funcion para realizar las operaciones
+    
     let numero1 = parseInt(document.getElementById("numero1").value); //Obtenemos el valor del primer numero del formulario
     let numero2 = parseInt(document.getElementById("numero2").value); //Obtenemos el valor del segundo numero del formulario
-    document.writeln("<h1>OPERARACIONES</h1>");
-    document.writeln("<br><br>El valor del primer numero es:" + numero1); //Mostramos el valor del primer numero
+    let operacion = document.getElementById("operacion").value; //Obtenemos el valor de la operacion del formulario
 
-    document.writeln("<br><br>El valor del segundo numero es:" +  numero2); //Mostramos el valor del segundo numero
-    let suma = numero1 + numero2; //Realizamos la suma de los dos numeros
-    document.writeln("<br><br>El valor de la suma: " + numero1 + " + " + numero2 + " = " + suma); //Mostramos el resultado de la suma
-
-    //Raiz cuadrada
-    let raiz = parseFloat(Math.sqrt(suma)); //Calculamos la raiz cuadrada del resultado de la suma
-    document.writeln("<br><br>La raiz cuadrada de " + suma + " es: " + raiz); //Mostramos el resultado de la raiz cuadrada
-
-    //SELECCIONES OPERACIONES 
-    if operacion =="+"{
-        let suma = numero1 + numero2;
-    } else if operacion =="-"{
-        let resta = numero1 - numero2;
+    //mostramos los valores obtenidos
+    document.writeln("<h1>OPERACIONES</h1>");
+    document.writeln("<br><br>El valor del primer numero es: " + numero1);
+    document.writeln("<br><br>El valor del segundo numero es: " + numero2); 
+    
+    if (operacion == "+"){ //cindicion para saber que operacion se va a realizar
+        let suma = numero1 + numero2; //Realizamos la operacion
+        document.writeln("<br><br>El valor de la suma: " + numero1 + " + " + numero2 + " = " + suma); //Mostramos el resultado
+    }else if (operacion == "-"){
+        let resta = numero1 - numero2; 
+        document.writeln("<br><br>El valor de la resta: " + numero1 + " - " + numero2 + " = " + resta); 
+    }else if (operacion == "*"){
+        let multiplicacion = numero1 * numero2;
+        document.writeln("<br><br>El valor de la multiplicacion: " + numero1 + " * " + numero2 + " = " + multiplicacion); 
+    }else if (operacion == "/"){
+        let division = numero1 / numero2;
+        document.writeln("<br><br>El valor de la division: " + numero1 + " / " + numero2 + " = " + division);
+    }else{
+        document.writeln("<br><br>Operacion no valida"); //Si no es ninguna de las operaciones anteriore
     }
-
-
+ 
 }
